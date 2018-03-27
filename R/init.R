dfa94f45eb956eef10e66845de7438739485021f
kisungyou <ksyou496@gmail.com>
2018-03-27 14:49:07 -0400

    initial;

---
+++ R/init.R
@@ -0,0 +1,3 @@
+.onAttach <- function(libname, pkgname){
+  packageStartupMessage("\n * Rlinsolve for Solving (Sparse) System of Linear Equations is authored by Kisung You (University of Notre Dame) under License GPL 3.\n\n * Underdeteremined system is currently not supported. Solvers with regularization constraints to be updated along further developments.")
+}
